06ba5c612ecdd422b90ef1a05d0f9653721dae3f
bytebeardigital <hello@bytebeardigital.com>
2021-11-06 22:25:54 -0400

    removed old files

--- byte-react/src/components/HeadingBlock.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const HeadingBlock = (props) => {
-    return  <div className="heading-block">
-    <h2 className="heading">{props.headingTitle}</h2>
-</div>;
-}
-
-export default HeadingBlock;
